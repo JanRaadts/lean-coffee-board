11267a2cdfe11520abe166b20586d4fad17a6dd0
Jan Raadts <janraadts@outlook.de>
2022-12-01 14:57:00 +0000

    deleted button is working

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import Image from "next/image";
+import { nanoid } from "nanoid";
+import { AiOutlinePlus } from "react-icons/ai";
 
 export default function Form({ entryData }) {
   function handleNewEntry(event) {
     event.preventDefault();
     let inputData = {
+      id: nanoid(),
       text: event.target.elements.varText.value,
       author: event.target.elements.author.value,
     };
@@ -30,7 +33,7 @@ export default function Form({ entryData }) {
         ></StyledInputAuthor>
         <StyledButton type="submit">
           {/* <Image src="/add-button.png" alt="button" width={1} height={1} /> */}
-          add
+          <AiOutlinePlus />
         </StyledButton>
       </StyledForm>
     </>
@@ -40,26 +43,36 @@ export default function Form({ entryData }) {
 const StyledForm = styled.form`
   position: absolute;
   bottom: 0;
+  background-color: white;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
 
 const StyledInputText = styled.input`
   padding: 5px;
   width: 250px;
-  height: 100px;
+  height: 60px;
   border: none;
+  border-bottom: 2px #0084b2 solid;
+  margin-right: 10px;
+  margin-left: 10px;
   font-size: 15px;
 `;
 
 const StyledInputAuthor = styled.input`
   padding: 5px;
-  height: 100px;
+  height: 60px;
   border: none;
+  border-bottom: 2px #0084b2 solid;
   font-size: 15px;
+  margin-bottom: 10px;
 `;
 
 const StyledButton = styled.button`
-  padding: 5px;
-  height: 100px;
   border: none;
-  width: 80px;
+  background-color: white;
+  font-size: 30px;
+  text-align: center;
+  margin-left: 10px;
 `;
